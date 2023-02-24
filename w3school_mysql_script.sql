a26105a6735d679247069c106b327034b4ff8cd2
nuth-vireak <timberr.4t@gmail.com>
2023-02-25 00:11:56 +0700

    mysql_where

--- w3school_mysql_script.sql
+++ w3school_mysql_script.sql
@@ -10,3 +10,47 @@ FROM w3schools.customers;
 SELECT DISTINCT
     Country
 FROM w3schools.customers;
+
+SELECT
+    COUNT(DISTINCT Country)
+FROM w3schools.customers;
+
+SELECT *
+FROM w3schools.customers
+WHERE Country = 'Mexico';
+
+SELECT *
+FROM w3schools.customers
+WHERE CustomerID = 1;
+
+SELECT *
+FROM w3schools.products
+WHERE Price = 18;
+
+SELECT *
+FROM w3schools.products
+WHERE Price > 30;
+
+SELECT *
+FROM w3schools.products
+WHERE Price < 30;
+
+SELECT *
+FROM w3schools.products
+WHERE Price >= 30;
+
+SELECT *
+FROM w3schools.products
+WHERE Price <= 30;
+
+SELECT *
+FROM w3schools.products
+WHERE Price BETWEEN 50 AND 60;
+
+SELECT *
+FROM w3schools.customers
+WHERE City LIKE 's%';
+
+SELECT *
+FROM w3schools.customers
+WHERE City IN ('Paris', 'London');
